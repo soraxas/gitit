6de0db277a42c8b417943dccd813f31e1e9819b9
Tin Lai <oscar@tinyiu.com>
2020-08-03 22:25:47 +1000

    improve preview js with real-time rendering
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- static/js/preview.js
+++ static/js/preview.js
@@ -29,20 +29,64 @@ $(document).ready(function() {
         '  <span>Preview</span>' +
         '</button>'
     );
+
+    // $('#previewpane').insertAfter('#editor');​
     $("#previewpane").replaceWith(
-        '<div id="preview-modal" class="modal fade">' +
-        '  <div class="modal-dialog modal-lg">' +
-        '    <div class="modal-content">' +
-        '      <div class="modal-header">' +
-        '        <p class="modal-title h4"><i class="fa fa-eye fa-fw mr-2"></i>Preview</p>' +
-        '        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-        '      </div>' +
-        '      <div class="modal-body">' +
-        '        <div id="previewpane" class=""></div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
+      '<div id="previewpane-wrapper">' +
+      '  <p class="title h4"><i class="fa fa-eye fa-fw mr-2"></i>Preview</p>' +
+      '  <div id="previewpane" class=""></div>' +
+      '</div>'
+      );
+
+    // move next to the editor (side by side)
+    $('#editor')[0].setAttribute("style", "float:left; width:50%; overflow:hidden;");
+    // style set it to half width, and make it a scroll box by the overflow attr
+    $('#previewpane-wrapper')[0].setAttribute("style", "width:50%; overflow:hidden;" + 
+                                               "overflow: auto; padding-left: 15px; max-height: 650px;");
+    $("#previewpane-wrapper").insertAfter("#editor");
+    // set ace editor to be the same height as the preview
+    $("#editor").height("650px");
+    editor.resize();
+    
+    //// ORIGINAL
+    // $("#previewpane").replaceWith(
+    //     '<div id="preview-modal" class="modal fade">' +
+    //     '  <div class="modal-dialog modal-lg">' +
+    //     '    <div class="modal-content">' +
+    //     '      <div class="modal-header">' +
+    //     '        <p class="modal-title h4"><i class="fa fa-eye fa-fw mr-2"></i>Preview</p>' +
+    //     '        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+    //     '      </div>' +
+    //     '      <div class="modal-body">' +
+    //     '        <div id="previewpane" class=""></div>' +
+    //     '      </div>' +
+    //     '    </div>' +
+    //     '  </div>' +
+    //     '</div>'
+    // );
+
     $('#editedText').focus();
+    updatePreviewPane();
+
+    // auto refresh every few second onChange event
+    let previewTimeout = 6000;
+    let previewCurrentlyOnTimeout = false;
+    let hasPendingPreviewUpdate = false;
+    editor.getSession().on("change", function () {
+      // restrict update too frequently
+      if(!previewCurrentlyOnTimeout) {
+        previewCurrentlyOnTimeout = true;
+        setTimeout(function () {
+          previewCurrentlyOnTimeout = false;
+          if (hasPendingPreviewUpdate) {
+            updatePreviewPane();
+            hasPendingPreviewUpdate = false;
+          }
+        }, previewTimeout);
+          
+        updatePreviewPane();
+      } else {
+        hasPendingPreviewUpdate = true;
+    }
+  });
 });
